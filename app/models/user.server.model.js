cd82a5d5cd1722a96ea237fd15e052c5d7ff008f
Qun  Yan <yq1119apzz@gmail.com>
2016-07-06 20:38:26 -0400

    add create user function in controller and route modify config file

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,14 +1,28 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
-UserSchema = new Schema({
-	firstname:String,
-	lastname:String,
-	username:{
-		type:String,
-		unique: true
-	},
-	password:String,
-	email:String,
-	zipcode:String
-})
+// UserSchema = new Schema({
+// 	firstname:String,
+// 	lastname:String,
+// 	username:{
+// 		type:String,
+// 		unique: true
+// 	},
+// 	password:String,
+// 	email:String,
+// 	zipcode:String
+// });
+
+// mongoose.model('User', UserSchema);
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    name: String,
+    email: String,
+    username: String,
+    password: String,
+});
+
+mongoose.model('User', UserSchema);
