86c62f5932c488058e69c783844943d60b861f8a
Qun  Yan <yq1119apzz@gmail.com>
2016-07-19 23:00:49 -0400

    add image popup feature

---
+++ public/js/shared/popup.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	// $(".popup").hide();
+
+	$(".popup").magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+});
