a15ccb913eb4eb55253127617fcc78ebab948688
Qun  Yan <yq1119apzz@gmail.com>
2016-07-19 23:10:55 -0400

    change popup style

--- public/js/shared/popup.js
+++ public/js/shared/popup.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
 	// $(".popup").hide();
 
-	$(".popup").magnificPopup({
+	// $(".popup").magnificPopup({
+	// 	type: 'image',
+	// 	closeOnContentClick: true,
+	// 	mainClass: 'mfp-img-mobile',
+	// 	image: {
+	// 		verticalFit: true
+	// 	}
+		
+	// });
+
+	$('.popup').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
 		image: {
 			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
 		}
-		
 	});
 });
